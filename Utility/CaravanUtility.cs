68a3477ff223b14a4b46f1c6054f0b8c494ee27b
IdealNightOcean <idealyehai@qq.com>
2025-07-12 19:02:27 +0800

    优化代码结构

--- Utility/CaravanUtility.cs
+++ Utility/CaravanUtility.cs
@@ -1,6 +1,7 @@
 ﻿using RimWorld.Planet;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using Verse;
 
@@ -8,21 +9,19 @@ namespace OberoniaAurea_Frame;
 
 public static class OAFrame_CaravanUtility
 {
-    public static bool IsExactTypeCaravan(object caravan)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static bool IsExactTypeCaravan<T>(T caravan) where T : Caravan
     {
-        if (caravan is null)
-        {
-            return false;
-        }
-        if (caravan.GetType() == typeof(Caravan))
+        if (caravan is not null && caravan.GetType() == typeof(Caravan))
         {
             return true;
         }
+
         Find.WindowStack.Add(Dialog_MessageBox.CreateConfirmation("OAFrame_WarningAbnormalCaravan".Translate(), null, destructive: false, title: "OAFrame_WarningAbnormalCaravanTitle".Translate()));
         return false;
     }
 
-    public static bool CaravanHasAnyThingsOfCategory(Caravan caravan, ThingCategoryDef thingCategoryDef, Func<Thing, bool> validator = null)
+    public static bool CaravanHasAnyThingsOfCategory(Caravan caravan, ThingCategoryDef thingCategoryDef, Predicate<Thing> validator = null)
     {
         List<Thing> list = CaravanInventoryUtility.AllInventoryItems(caravan);
         for (int i = 0; i < list.Count; i++)
@@ -35,7 +34,8 @@ public static class OAFrame_CaravanUtility
         }
         return false;
     }
-    public static bool CaravanHasEnoughThingsOfCategory(Caravan caravan, ThingCategoryDef thingCategoryDef, int count, Func<Thing, bool> validator = null)
+
+    public static bool CaravanHasEnoughThingsOfCategory(Caravan caravan, ThingCategoryDef thingCategoryDef, int count, Predicate<Thing> validator = null)
     {
         int num = 0;
         List<Thing> list = CaravanInventoryUtility.AllInventoryItems(caravan);
@@ -45,29 +45,36 @@ public static class OAFrame_CaravanUtility
             if (thing.def.thingCategories.Contains(thingCategoryDef) && (validator is null || validator(thing)))
             {
                 num += thing.stackCount;
+                if (num >= count)
+                {
+                    return true;
+                }
             }
         }
-        return num >= count;
+        return false;
     }
 
     public static int RemoveThings(Caravan caravan, ThingDef thingDef, int count)
     {
         int remaining = count;
-        List<Thing> takeThings = CaravanInventoryUtility.TakeThings(caravan, delegate (Thing thing)
-        {
-            if (thingDef != thing.def)
-            {
-                return 0;
-            }
-            int takeCount = Mathf.Min(remaining, thing.stackCount);
-            remaining -= takeCount;
-            return takeCount;
-        });
+        List<Thing> takeThings = CaravanInventoryUtility.TakeThings(caravan, GetTakeThingCount);
         for (int i = 0; i < takeThings.Count; i++)
         {
             takeThings[i].Destroy();
         }
 
         return remaining;
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        int GetTakeThingCount(Thing thing)
+        {
+            if (remaining <= 0 || thingDef != thing.def)
+            {
+                return 0;
+            }
+            int takeCount = Mathf.Min(remaining, thing.stackCount);
+            remaining -= takeCount;
+            return takeCount;
+        }
     }
 }
