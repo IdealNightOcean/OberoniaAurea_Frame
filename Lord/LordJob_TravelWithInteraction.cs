78b7c6ec3b2a5f3aecf79140179b4439d0ff99d4
IdealNightOcean <idealyehai@qq.com>
2026-05-15 19:23:03 +0800

    26.05.15 feat(Lord): 添加带交互的旅行职责及相关逻辑
    1. 新增LordJob_TravelWithInteraction职责作业类，基于旅行职责扩展交互逻辑
    2. 在项目配置中新增对应编译文件
    3. 为FixedCaravan新增Humanlike pawn相关的枚举和计数属性
    4. 重写VisitColonyBase的状态图逻辑，使用新的带交互旅行职责

---
+++ Lord/LordJob_TravelWithInteraction.cs
@@ -0,0 +1,43 @@
+﻿using Verse;
+using Verse.AI.Group;
+
+namespace OberoniaAurea_Frame;
+
+public class LordJob_TravelWithInteraction : LordJob_Travel
+{
+    protected IntVec3 interactTravelDest;
+
+    public LordJob_TravelWithInteraction() { }
+
+    public LordJob_TravelWithInteraction(IntVec3 travelDest) : base(travelDest)
+    {
+        interactTravelDest = travelDest;
+    }
+
+    public override StateGraph CreateGraph()
+    {
+        StateGraph stateGraph = new();
+        LordToil_TravelWithInteraction lordToil_Travel = new(interactTravelDest);
+        stateGraph.StartingToil = lordToil_Travel;
+        LordToil_DefendPoint lordToil_DefendPoint = new(canSatisfyLongNeeds: false);
+        stateGraph.AddToil(lordToil_DefendPoint);
+
+        Transition transition1 = new(lordToil_Travel, lordToil_DefendPoint);
+        transition1.AddTrigger(new Trigger_PawnHarmed());
+        transition1.AddPreAction(new TransitionAction_SetDefendLocalGroup());
+        transition1.AddPostAction(new TransitionAction_EndAllJobs());
+        stateGraph.AddTransition(transition1);
+
+        Transition transition2 = new(lordToil_DefendPoint, lordToil_Travel);
+        transition2.AddTrigger(new Trigger_TicksPassedWithoutHarm(1200));
+        transition2.AddPreAction(new TransitionAction_EnsureHaveExitDestination());
+        stateGraph.AddTransition(transition2);
+        return stateGraph;
+    }
+
+    public override void ExposeData()
+    {
+        base.ExposeData();
+        Scribe_Values.Look(ref interactTravelDest, nameof(interactTravelDest));
+    }
+}
--- Lord/LordJob_VisitColonyBase.cs
+++ Lord/LordJob_VisitColonyBase.cs
@@ -41,6 +41,122 @@ public class LordJob_VisitColonyBase : LordJob, ILordFloatMenuProvider
     public override StateGraph CreateGraph()
     {
         StateGraph stateGraph = new();
+        StateGraph travelSubgraph = new LordJob_TravelWithInteraction(chillSpot).CreateGraph();
+        LordToil lordToil_TravelWithInteraction = travelSubgraph.StartingToil;
+        stateGraph.StartingToil = lordToil_TravelWithInteraction;
+
+        LordToil_DefendPoint lordToil_DefendPoint = new(chillSpot);
+        stateGraph.AddToil(lordToil_DefendPoint);
+        LordToil_TakeWoundedGuest lordToil_TakeWoundedGuest = new();
+        stateGraph.AddToil(lordToil_TakeWoundedGuest);
+
+        StateGraph exitSubgraph = new LordJob_TravelAndExit(IntVec3.Invalid).CreateGraph();
+        stateGraph.AttachSubgraph(exitSubgraph);
+        LordToil lordToil_ExitSubgraph_Start = exitSubgraph.StartingToil;
+        LordToil lordToil_ExitSubgraph_Target = exitSubgraph.lordToils[1];
+
+        LordToil_ExitMap lordToil_ExitMap = new(LocomotionUrgency.Sprint, canDig: true);
+        stateGraph.AddToil(lordToil_ExitMap);
+
+        Transition transition1 = new(lordToil_TravelWithInteraction, lordToil_ExitSubgraph_Start);
+        transition1.AddSources(lordToil_DefendPoint);
+        transition1.AddTrigger(new Trigger_PawnExperiencingDangerousTemperatures());
+        if (faction is not null)
+        {
+            transition1.AddPreAction(new TransitionAction_Message("MessageVisitorsDangerousTemperature".Translate(faction.def.pawnsPlural.CapitalizeFirst(), faction.Name)));
+        }
+        transition1.AddPreAction(new TransitionAction_EnsureHaveExitDestination());
+        transition1.AddPostAction(new TransitionAction_EndAllJobs());
+        stateGraph.AddTransition(transition1);
+
+        Transition transition2 = new(lordToil_TravelWithInteraction, lordToil_ExitSubgraph_Start);
+        transition2.AddSources(lordToil_DefendPoint);
+        transition2.AddTrigger(new Trigger_PawnExperiencingAnomalousWeather());
+        if (faction is not null)
+        {
+            transition2.AddPreAction(new TransitionAction_Message("MessageVisitorsAnomalousWeather".Translate(faction.def.pawnsPlural.CapitalizeFirst(), faction.Name)));
+        }
+        transition2.AddPreAction(new TransitionAction_EnsureHaveExitDestination());
+        transition2.AddPostAction(new TransitionAction_EndAllJobs());
+        stateGraph.AddTransition(transition2);
+
+        Transition transition3 = new(lordToil_TravelWithInteraction, lordToil_ExitMap);
+        transition3.AddSources(lordToil_DefendPoint, lordToil_TakeWoundedGuest);
+        transition3.AddSources(exitSubgraph.lordToils);
+        transition3.AddTrigger(new Trigger_PawnCannotReachMapEdge());
+        if (faction is not null)
+        {
+            transition3.AddPreAction(new TransitionAction_Message("MessageVisitorsTrappedLeaving".Translate(faction.def.pawnsPlural.CapitalizeFirst(), faction.Name)));
+        }
+        stateGraph.AddTransition(transition3);
+
+        Transition transition4 = new(lordToil_ExitMap, lordToil_ExitSubgraph_Start);
+        transition4.AddTrigger(new Trigger_PawnCanReachMapEdge());
+        transition4.AddPreAction(new TransitionAction_EnsureHaveExitDestination());
+        transition4.AddPostAction(new TransitionAction_EndAllJobs());
+        stateGraph.AddTransition(transition4);
+
+        Transition transition5 = new(lordToil_TravelWithInteraction, lordToil_DefendPoint);
+        transition5.AddTrigger(new Trigger_Memo("TravelArrived"));
+        stateGraph.AddTransition(transition5);
+        if (faction is not null)
+        {
+            Transition transition6 = new(lordToil_DefendPoint, lordToil_TakeWoundedGuest);
+            transition6.AddTrigger(new Trigger_WoundedGuestPresent());
+            transition6.AddPreAction(new TransitionAction_Message("MessageVisitorsTakingWounded".Translate(faction.def.pawnsPlural.CapitalizeFirst(), faction.Name)));
+            stateGraph.AddTransition(transition6);
+        }
+
+        Transition transition7 = new(lordToil_DefendPoint, lordToil_ExitSubgraph_Target);
+        transition7.AddSources(lordToil_TakeWoundedGuest, lordToil_TravelWithInteraction);
+        transition7.AddTrigger(new Trigger_BecamePlayerEnemy());
+        transition7.AddPreAction(new TransitionAction_SetDefendLocalGroup());
+        transition7.AddPostAction(new TransitionAction_WakeAll());
+        transition7.AddPostAction(new TransitionAction_EndAllJobs());
+        stateGraph.AddTransition(transition7);
+
+        Transition transition8 = new(lordToil_DefendPoint, lordToil_ExitSubgraph_Start);
+        int tickLimit = ((!DebugSettings.instantVisitorsGift || faction is null) ? (durationTicks ?? Rand.Range(8000, 22000)) : 0);
+        transition8.AddTrigger(new Trigger_TicksPassed(tickLimit));
+        transition8.AddTrigger(new Trigger_Custom(s => s.type == TriggerSignalType.Tick && dismissed));
+        if (faction is not null)
+        {
+            transition8.AddPreAction(new TransitionAction_Message("VisitorsLeaving".Translate(faction.Name)));
+        }
+        if (gifts is not null)
+        {
+            transition8.AddPreAction(new TransitionAction_GiveGift
+            {
+                gifts = gifts
+            });
+        }
+        else
+        {
+            transition8.AddPreAction(new TransitionAction_CheckGiveGift());
+        }
+        transition8.AddPostAction(new TransitionAction_WakeAll());
+        transition8.AddPreAction(new TransitionAction_EnsureHaveExitDestination());
+        stateGraph.AddTransition(transition8);
+        return stateGraph;
+    }
+
+    public override void ExposeData()
+    {
+        Scribe_References.Look(ref faction, nameof(faction));
+        Scribe_Values.Look(ref chillSpot, nameof(chillSpot));
+        Scribe_Values.Look(ref durationTicks, nameof(durationTicks));
+        Scribe_Values.Look(ref dismissed, nameof(dismissed));
+        Scribe_Collections.Look(ref gifts, nameof(gifts), LookMode.Deep);
+        if (Scribe.mode == LoadSaveMode.PostLoadInit)
+        {
+            gifts?.RemoveAll(t => t is null);
+        }
+    }
+
+    /*
+    private StateGraph CreateGraphOld()
+    {
+        StateGraph stateGraph = new();
         LordToil_TravelWithInteraction lordToil_Travel = new(chillSpot);
         stateGraph.StartingToil = lordToil_Travel;
         LordToil_DefendPoint lordToil_DefendOnTheSpot = new(canSatisfyLongNeeds: false);
@@ -146,17 +262,5 @@ public class LordJob_VisitColonyBase : LordJob, ILordFloatMenuProvider
 
         return stateGraph;
     }
-
-    public override void ExposeData()
-    {
-        Scribe_References.Look(ref faction, nameof(faction));
-        Scribe_Values.Look(ref chillSpot, nameof(chillSpot));
-        Scribe_Values.Look(ref durationTicks, nameof(durationTicks));
-        Scribe_Values.Look(ref dismissed, nameof(dismissed));
-        Scribe_Collections.Look(ref gifts, nameof(gifts), LookMode.Deep);
-        if (Scribe.mode == LoadSaveMode.PostLoadInit)
-        {
-            gifts?.RemoveAll(t => t is null);
-        }
-    }
+    */
 }
